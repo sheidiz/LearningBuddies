289f42fc5b2cf56a348260935bc29e59064ab6e0
Sheila Diz <dizsheila07@gmail.com>
2024-06-23 19:09:29 -0300

    feat: create base springboot project
    - added dependencies: spring web, tomcat, jsp, jstl, mysql, spring jpa,
    spring starter validation, jbcrypt

---
+++ src/main/java/com/sheiladiz/learningbuddies/LearningBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.sheiladiz.learningbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningBuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheiladiz/learningbuddies/LearningBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheiladiz.learningbuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
